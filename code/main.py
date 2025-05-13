ab9186992b0bf2ec1f5c43ea838f06354525f0f4
Ali Iqbal <aliiqbal11502@gmail.com>
2025-05-13 16:34:45 +0500

    commit

--- code/main.py
+++ code/main.py
@@ -19,39 +19,24 @@ def generate_highlights(video, language='english'):
     model_name = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
     threshold_similarity = 0.2
 
-    start = time.time()
     words_data = read_word_data(formatted_transcript_path)
-    print("read_word_data:", time.time() - start, "seconds")
 
     if language == 'english':
-        start = time.time()
         sentences_timestamps = process_transcript_words(words_data, output_file=formatted_sentences_counts_path)
-        print("process_transcript_words:", time.time() - start, "seconds")
     else:
-        start = time.time()
         sentences_timestamps = segment_by_pauses(words_data)
-        print("segment_by_pauses:", time.time() - start, "seconds")
 
-    start = time.time()
     sentences_timestamps_chunked = chunk_subtitle_segments(model_name, sentences_timestamps, threshold_similarity)
-    print("chunk_subtitle_segments:", time.time() - start, "seconds")
 
-    start = time.time()
     sentences_timestamps_chunked = filter_by_word_count(sentences_timestamps_chunked, 6)
-    print("filter_by_word_count:", time.time() - start, "seconds")
 
-    start = time.time()
     summary = merge_extractive_summaries(model_name, sentences_timestamps_chunked, summary_ratio=0.25,
                                          min_topic_size=2, mmr_lambda_english=0.7, language=language)
-    print("merge_extractive_summaries:", time.time() - start, "seconds")
 
-    start = time.time()
+
     save_extractive_summary(extracted_summary_path, summary=summary, merged=True)
-    print("save_extractive_summary:", time.time() - start, "seconds")
 
-    start = time.time()
     process_video(summary, video_path, output_path)
-    print("process_video:", time.time() - start, "seconds")
 
     total_end = time.time()
     print("Total execution time:", total_end - total_start, "seconds")
@@ -60,7 +45,7 @@ def generate_highlights(video, language='english'):
 
 if __name__ == "__main__":
     
-    generate_highlights('uiC3mhmh8AQ_30minutes',language='english')    
+    generate_highlights('Ti5vfu9arXQ_25minutes',language='english')    
     #uiC3mhmh8AQ_30minutes
     #Ti5vfu9arXQ_25minutes
     #6vX3Us1TOw8_14minutes
